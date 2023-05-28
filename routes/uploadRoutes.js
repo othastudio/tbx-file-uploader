444d71eac71b4dbff5e9dbb9547f2051eb02b320
Othmane N <otmannj56@gmail.com>
2023-05-28 21:03:41 +0100

    ship tastybox-file-uploader 1.0 🚀

--- routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const uploadController = require('../controllers/uploadController');
-const imageView = require('../views/imageView');
-
-router.post('/', uploadController.uploadImage, imageView.getImagePath);
-
-module.exports = router;
--- views/imageView.js
+++
@@ -1,5 +0,0 @@
-function getImagePath(req, res) {
-  res.json({ path: `/images/${req.file.filename}` });
-}
-
-module.exports = { getImagePath };
