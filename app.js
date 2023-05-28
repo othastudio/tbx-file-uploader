d0f704b258d6278054090112035787ed5c26b3d1
Othmane N <otmannj56@gmail.com>
2023-05-28 20:50:29 +0100

    return the structure to mvc pattern

--- app.js
+++ app.js
@@ -1,54 +1,17 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
 const app = express();
+const path = require('path');
 
-// Set up storage for uploaded files
-const storage = multer.diskStorage({
-  destination: './uploads',
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
+const uploadRoutes = require('./routes/uploadRoutes');
 
-// Create multer upload instance
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 } // Adjust file size limit as needed
-}).single('image'); // 'image' should match the field name in the form data
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// Define the route for file upload
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      // Handle any upload errors
-      return res.status(500).json({ error: err.message });
-    }
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
-    // Return the file path
-    res.json({ path: req.file.path });
-  });
-});
+app.use('/upload', uploadRoutes);
 
-// Define the route to serve uploaded images
-app.get('/images/:filename', (req, res) => {
-  const { filename } = req.params;
-  const imagePath = path.join(__dirname, 'uploads', filename);
-
-  // Check if the file exists
-  if (fs.existsSync(imagePath)) {
-    // Read the file as a stream and pipe it to the response
-    const stream = fs.createReadStream(imagePath);
-    stream.pipe(res);
-  } else {
-    res.status(404).json({ error: 'Image not found' });
-  }
-});
-
-// Start the server
-const port = 3333; // Choose a port number
+const port = 3333;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
