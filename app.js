1b4e1edb28aa3bf4921186f7d47a8f401b16e5f2
Othmane N <otmannj56@gmail.com>
2023-05-28 21:02:32 +0100

    ship tastybox-file-uploader 1.0 🚀

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const uploadRoutes = require('./routes/uploadRoutes');
+
+const port = process.env.PORT || 3333;
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,7 +14,6 @@ app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/upload', uploadRoutes);
 
-const port = 3333;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on  ${process.env.HOST}:${port} 🚀`);
 });
